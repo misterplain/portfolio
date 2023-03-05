cdeabaa7d762928eb958b85d06a2383511072262
Patrick O'Brien <obrienp2321@gmail.com>
2023-03-05 14:32:54 +0100

    in progress and active server info added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -16,19 +16,64 @@ const Projects = () => {
       <Typography sx={styles.title} variant='body1' marginBottom>
         Projects
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-      
-            {" "}
-            <Project project={projects[0]} />
-      
+      <Grid container sx={styles.noticeWrapper}>
+        <Grid item xs={8} md={6} sx={styles.inProgressWrapper}>
+          <Box sx={styles.inProgressItem}>
+            <Box sx={styles.inProgressItemWrapper}>
+              {" "}
+              <Typography variant='body1' sx={styles.bodyText}>
+                My first paid freelance project is to make an ECommerce site for
+                my clients stained glass company. The site is 
+                <Link
+                  href='https://brackets-kz4n.onrender.com/'
+                  target='_blank'
+                  rel='noopener'
+                  style={{ textDecoration: "none", color: "#777FEB" }}
+                >
+                  {" "}deployed{" "}
+                </Link>
+                with the ongoing task list in
+                <Link
+                  href='https://github.com/misterplain/brackets'
+                  target='_blank'
+                  rel='noopener'
+                  style={{ textDecoration: "none", color: "#777FEB" }}
+                >
+                  {" "}README.md{" "}
+                </Link>
+                for easy collaboration
+              </Typography>
+            </Box>
+          </Box>
         </Grid>
-        <Grid item xs={12} md={6}>
-   
-            <Project project={projects[1]} />
-   
+        <Grid item xs={8} md={6} sx={styles.activeServerWrapper}>
+          <Box sx={styles.activeServerItem}>
+            {" "}
+            <Box sx={styles.activeServerItemWrapper}>
+              <Typography sx={styles.bodyText}>
+                To get around the usage limits of the free-tier of render.com,
+                where I am hosting these apps, I've created one active backend
+                server for all projects which can be found                 <Link
+                  href='https://github.com/misterplain/activeServer'
+                  target='_blank'
+                  rel='noopener'
+                  style={{ textDecoration: "none", color: "#777FEB" }}
+                >here</Link>
+              </Typography>
+            </Box>
+          </Box>
         </Grid>
       </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          {" "}
+          <Project project={projects[0]} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Project project={projects[1]} />
+        </Grid>
+      </Grid>
+
       <Box sx={styles.buttonWrapper}>
         <Link
           component={NavLink}
--- src/components/Projects/styles.js
+++ src/components/Projects/styles.js
@@ -1,18 +1,99 @@
 const styles = {
-    wrapper: (theme) => ({
-        display: "flex",
-        flexDirection: "column",
-        padding: "15px 15px",
-    }),
-    title: (theme) => ({
-        fontWeight: 700,
-    }),
-    buttonWrapper: (theme) => ({
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        marginTop: "20px",
-    })
-}
+  wrapper: (theme) => ({
+    display: "flex",
+    flexDirection: "column",
+    padding: "15px 15px",
+  }),
+  title: (theme) => ({
+    fontWeight: 700,
+  }),
+
+  noticeWrapper: (theme) => ({
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: "50px",
+  }),
+  inProgressWrapper: (theme) => ({
+    minHeight: "180px",
+    borderRadius: "5px",
+    backgroundImage:
+      "linear-gradient(45deg, #000000 25%, #f5d105 25%, #f5d105 50%, #000000 50%, #000000 75%, #f5d105 75%, #f5d105 100%)",
+    backgroundSize: "20.00px 20.00px",
+    position: "relative",
+    [theme.breakpoints.up("md")]: {
+        minHeight: "140px",
+    }
+  }),
+  inProgressItem: (theme) => ({
+    // minHeight: "100%",
+    borderRadius: "5px",
+    background: "white",
+    position: "absolute",
+    top: 5,
+    left: 5,
+    right: 5,
+    bottom: 5,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  }),
+  inProgressItemWrapper: (theme) => ({
+    display: "inline-block",
+  //  flexDirection: "row",
+  //   justifyContent: "center",
+  //   alignItems: "center",
+    textAlign: "center",
+    
+  }),
+  bodyText: (theme) => ({
+    // margin: "5px 10px",
+    // display: "inline-block",
+  }),
+  activeServerWrapper: (theme) => ({
+    minHeight: "180px",
+    borderRadius: "5px",
+    backgroundImage:
+      "linear-gradient(45deg, #dfe3df 25%, #777feb 25%, #777feb 50%, #dfe3df 50%, #dfe3df 75%, #777feb 75%, #777feb 100%)",
+    backgroundSize: "20.00px 20.00px",
+    position: "relative",
+    [theme.breakpoints.up("md")]: {
+        minHeight: "140px",
+    }
+  }),
+  activeServerItem: (theme) => ({
+    // minHeight: "100%",
+    borderRadius: "5px",
+    background: "white",
+    position: "absolute",
+    top: 5,
+    left: 5,
+    right: 5,
+    bottom: 5,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  }),
+  activeServerItemWrapper: (theme) => ({
+    // borderRadius: "5px",
+    // background: "white",
+    // position: "absolute",
+    // top: 5,
+    // left: 5,
+    // right: 5,
+    // bottom: 5,
+    display: "flex",
+    flexDirection: "column",
+     justifyContent: "center",
+     alignItems: "center",
+        textAlign: "center",
+  }),
+  buttonWrapper: (theme) => ({
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: "20px",
+  }),
+};
 
 export default styles;
